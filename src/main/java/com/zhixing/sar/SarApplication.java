72f5b42149524671ae5b4c12422196452d961580
Administrator <chenhaoyes>
2018-07-25 22:17:37 +0800

    初始化项目代码

---
+++ src/main/java/com/zhixing/sar/SarApplication.java
@@ -0,0 +1,12 @@
+package com.zhixing.sar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarApplication.class, args);
+	}
+}
